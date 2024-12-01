61e3081f8a0b4f11d8f19cfe4c261a1854a49245
Vinimms <viniciusilva145@gmail.com>
2024-12-01 19:33:31 -0300

    Estilização da MainScreen e implementação da SplashScreen

--- app/src/main/java/com/loc/composebiometricauth/MainActivity.kt
+++ app/src/main/java/com/loc/composebiometricauth/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.loc.composebiometricauth
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -21,8 +24,13 @@ import com.loc.composebiometricauth.ui.screens.CadastralAuthenticationScreen
 import com.loc.composebiometricauth.ui.theme.ComposeBiometricAuthTheme
 import com.loc.composebiometricauth.ui.screens.ScoreAntifraudeForm
 import com.loc.composebiometricauth.ui.screens.SimValidationScreen
+import com.loc.composebiometricauth.ui.screens.SplashScreen
+import com.loc.composebiometricauth.ui.theme.BackColor
+import com.loc.composebiometricauth.ui.theme.TxtColor
 
 class MainActivity : FragmentActivity() {
+    private var showSplashScreen by mutableStateOf(true)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,9 +40,15 @@ class MainActivity : FragmentActivity() {
             ComposeBiometricAuthTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = BackColor
                 ) {
-                    AppNavigator(biometricAuthenticator)
+                    if (showSplashScreen){
+                        SplashScreen (onTimeout = {
+                            showSplashScreen = false
+                        })
+                    } else {
+                        AppNavigator(biometricAuthenticator)
+                    }
                 }
             }
         }
@@ -55,52 +69,195 @@ fun AppNavigator(biometricAuthenticator: BiometricAuthenticator) {
 
 @Composable
 fun MainScreen(navController: NavController, biometricAuthenticator: BiometricAuthenticator) {
-    var biometricMessage by remember { mutableStateOf("") }
-
-    Column(
+    Surface(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = { navController.navigate("document_analysis") }) {
-            Text(text = "Análise de Documentos")
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = { navController.navigate("sim_swap") }) {
-            Text(text = "SIM SWAP")
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = { navController.navigate("cadastral_authentication") }) {
-            Text(text = "Autenticação Cadastral")
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = { navController.navigate("fraud_score") }) {
-            Text(text = "Score Antifraude")
-        }
-        Spacer(modifier = Modifier.height(20.dp))
-        Button(
-            onClick = {
-                biometricAuthenticator.promptBiometricAuth(
-                    title = "Login Biométrico",
-                    subTitle = "Use sua impressão digital para autenticar",
-                    negativeButtonText = "Cancelar",
-                    fragmentActivity = navController.context as FragmentActivity,
-                    onSuccess = {
-                        biometricMessage = "Autenticação bem-sucedida!"
-                    },
-                    onError = { _, errorString ->
-                        biometricMessage = "Erro: $errorString"
-                    },
-                    onFailed = {
-                        biometricMessage = "Autenticação falhou."
-                    }
-                )
-            }
+        color = BackColor
+    ){
+        var biometricMessage by remember { mutableStateOf("") }
+
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(
+                    vertical = 70.dp,
+                    horizontal = 25.dp
+                ),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Autenticar com Biometria")
+            Button(onClick = { navController.navigate("document_analysis") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Start)
+                    .height(55.dp)
+                    .shadow(
+                        2.dp,
+                        shape = RoundedCornerShape(8.dp),
+                        spotColor = Color.Black
+                    )
+                    .clip(RoundedCornerShape(8.dp))
+                ,colors = ButtonDefaults.buttonColors(Color.White),
+                shape = RoundedCornerShape(8.dp)
+            )
+            {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                )
+                {
+                    Text(text = "Análise de Documentos",
+                        color = TxtColor,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                    )
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = { navController.navigate("sim_swap") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Start)
+                    .height(55.dp)
+                    .shadow(
+                        2.dp,
+                        shape = RoundedCornerShape(8.dp),
+                        spotColor = Color.Black
+                    )
+                    .clip(RoundedCornerShape(8.dp))
+                ,colors = ButtonDefaults.buttonColors(Color.White),
+                shape = RoundedCornerShape(8.dp)
+            )
+            {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                )
+                {
+                    Text(
+                        text = "SIM SWAP",
+                        color = TxtColor,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = { navController.navigate("cadastral_authentication")  },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Start)
+                    .height(55.dp)
+                    .shadow(
+                        2.dp,
+                        shape = RoundedCornerShape(8.dp),
+                        spotColor = Color.Black
+                    )
+                    .clip(RoundedCornerShape(8.dp))
+                ,colors = ButtonDefaults.buttonColors(Color.White),
+                shape = RoundedCornerShape(8.dp)
+            )
+            {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    Text(text = "Autenticação Cadastral",
+                        color = TxtColor,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center)
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = { navController.navigate("fraud_score") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Start)
+                    .height(55.dp)
+                    .shadow(
+                        2.dp,
+                        shape = RoundedCornerShape(8.dp),
+                        spotColor = Color.Black
+                    )
+                    .clip(RoundedCornerShape(8.dp))
+                ,colors = ButtonDefaults.buttonColors(Color.White),
+                shape = RoundedCornerShape(8.dp)
+            )
+            {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                )
+                {
+                    Text(text = "Score Antifraude",
+                        color = TxtColor,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center)
+                }
+
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = {
+                    biometricAuthenticator.promptBiometricAuth(
+                        title = "Login Biométrico",
+                        subTitle = "Use sua impressão digital para autenticar",
+                        negativeButtonText = "Cancelar",
+                        fragmentActivity = navController.context as FragmentActivity,
+                        onSuccess = {
+                            biometricMessage = "Autenticação bem-sucedida!"
+                        },
+                        onError = { _, errorString ->
+                            biometricMessage = "Erro: $errorString"
+                        },
+                        onFailed = {
+                            biometricMessage = "Autenticação falhou."
+                        }
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Start)
+                    .height(55.dp)
+                    .shadow(
+                        2.dp,
+                        shape = RoundedCornerShape(8.dp),
+                        spotColor = Color.Black
+                    )
+                    .clip(RoundedCornerShape(8.dp))
+                ,colors = ButtonDefaults.buttonColors(Color.White),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start
+                )
+                {
+                    Text(text = "Autenticar com Biometria",
+                        color = TxtColor,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center)
+                }
+
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(text = biometricMessage)
         }
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(text = biometricMessage)
     }
 }
 
@@ -114,5 +271,11 @@ fun DocumentAnalysisScreen() {
     }
 }
 
-
+/*
+@Preview (showBackground = true)
+@Composable
+fun PrevieMainScreen (){
+    MainScreen()
+}
+*/
 
